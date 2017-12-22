8b880318ad597b0de947ef974fe0c0b9c40663f3
Konrad Linkowski <linkowski.konrad97@gmail.com>
2017-12-22 14:08:30 +0100

    Api test

---
+++ test_server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 8081;
+const axios = require('axios');
+app.get('/bilety', function(req, res) {
+    
+})
+
+app.listen(port);
